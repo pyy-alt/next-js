c8f032ddeb198039bd88dc6d8b4d7c7dc2d24220
zhangdabao <18234101300@163.com>
2025-02-27 16:36:01 +0800

    add： 官网链接

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,10 @@
 // Copyright (c) 2025 zdb
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+
+// 具体参考官网
+// https://orm.drizzle.team/docs/get-started/neon-new
+
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon, neonConfig } from '@neondatabase/serverless';
 import { config } from 'dotenv';
